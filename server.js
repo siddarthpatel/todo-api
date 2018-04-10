4294428d91aeb5881188433ab218a5e27f8bb3bd
Siddarth Patel <siddarthpatel97@gmail.com>
2018-04-10 16:58:49 +0530

    add get todo in server.js

--- server.js
+++ server.js
@@ -18,12 +18,31 @@ var todos = [{
 }
 ];
 
+function fetchTodo(todoId){
+	var todo;
+	for (var i=0; i<todos.length; i++){
+		if(todos[i].id === todoId){
+			todo = todos[i];
+			return todo;
+		}
+	}
+}
+
+app.get('/', function(req, res){
+	res.send('Todo API root');
+});
+
 app.get('/todos', function(req, res){
 	res.json(todos);
 });
 
-app.get('/', function(req, res){
-	res.send('Todo API root');
+app.get('/todos/:id', function(req, res){
+	var todo = fetchTodo(parseInt(req.params.id, 10));
+
+	if (todo)
+		res.json(todo);
+	else
+		res.status(404).send();
 });
 
 app.listen(PORT, function(){
