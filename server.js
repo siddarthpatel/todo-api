c62649dcdea503549d1718846b4b1e7de507aabe
Siddarth Patel <siddarthpatel97@gmail.com>
2018-04-10 16:13:59 +0530

    add todos collection to server.js

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 var express = require('express');
-
 var app = express();
-
 var PORT = process.env.PORT || 3000;
+
+var todos = [{
+	id:1,
+	description: "Meet mom for lunch",
+	completed: false
+},{
+	id:2,
+	description: "Write a one page paper",
+	completed: false
+},
+{
+	id:3,
+	description: "Walk the dog",
+	completed: true
+}
+];
+
+app.get('/todos', function(req, res){
+	res.json(todos);
+});
 
 app.get('/', function(req, res){
 	res.send('Todo API root');
