0777742466aeba5d9fed4f9af4ff672d126a9be8
Siddarth Patel <siddarthpatel97@gmail.com>
2018-04-14 13:09:49 +0530

    add create todo

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
-var todos = [{
+var nextId = 1;
+var todos = [/*{
 	id:1,
 	description: "Meet mom for lunch",
 	completed: false
@@ -15,7 +18,7 @@ var todos = [{
 	id:3,
 	description: "Walk the dog",
 	completed: true
-}
+}*/
 ];
 
 function fetchTodo(todoId){
@@ -28,14 +31,23 @@ function fetchTodo(todoId){
 	}
 }
 
+function addTodo(todo){
+	todo.id = nextId;
+	todos.push(todo);
+	nextId = nextId + 1;
+	return todo;
+}
+
 app.get('/', function(req, res){
 	res.send('Todo API root');
 });
 
+//GET/todos
 app.get('/todos', function(req, res){
 	res.json(todos);
 });
 
+//GET/todos/id
 app.get('/todos/:id', function(req, res){
 	var todo = fetchTodo(parseInt(req.params.id, 10));
 
@@ -43,6 +55,13 @@ app.get('/todos/:id', function(req, res){
 		res.json(todo);
 	else
 		res.status(404).send();
+});
+
+//POST/todos
+app.post('/todos', function(req, res){
+	var body = req.body; // parsed by bodyParse as JSON to read data in server, and then retured as an Object in req.body
+	var todo = addTodo(body)
+	res.json(body);
 });
 
 app.listen(PORT, function(){
