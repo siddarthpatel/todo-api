4042c83cef93d3696397940796bde50483b73caf
Siddarth Patel <siddarthpatel97@gmail.com>
2018-04-23 14:36:25 +0530

    add PUT route

--- server.js
+++ server.js
@@ -46,14 +46,14 @@ app.get('/todos/:id', function(req, res){
 
 //POST /todos
 app.post('/todos', function(req, res){
-	var body = _.pick(req.body, 'description', 'completed'); // parsed by bodyParse as JSON to read data in server, and then retured as an Object in req.body
+	var body = _.pick(req.body, 'description', 'completed'); // parsed by bodyParser as JSON to read data in server, and then retured as an Object in req.body
 	
 	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length == 0)
 		return res.status(404).send();
 
 	body.description = body.description.trim();
 	var todo = addTodo(body)
-	res.json(body);
+	res.json(todo);
 });
 
 //DELETE /todos/:id
@@ -68,6 +68,28 @@ app.delete('/todos/:id', function(req, res){
 	} 
 	else
 		res.status(404).send();
+});
+
+//PUT /todos/:id
+app.put('/todos/:id', function(req, res){
+	var matchedTodo = fetchTodo(parseInt(req.params.id, 10))
+	var body = _.pick(req.body, 'description', 'completed'); // parsed by bodyParse as JSON to read data in server, and then retured as an Object in req.body
+	var validAttributes = {};
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed'))
+		return res.status(400).send();
+	
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')){
+		return res.status(400).send();
+	}
+
+	_.extend(matchedTodo, validAttributes)
+	res.json(matchedTodo);
 });
 
 app.listen(PORT, function(){
