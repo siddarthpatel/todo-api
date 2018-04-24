d46c26162f263297a2f4a17b5e37d86f7928ece9
Siddarth Patel <siddarthpatel97@gmail.com>
2018-04-24 14:32:30 +0530

    add completed query params

--- server.js
+++ server.js
@@ -29,9 +29,17 @@ app.get('/', function(req, res){
 	res.send('Todo API root');
 });
 
-//GET /todos
+//GET /todos?completed=true
 app.get('/todos', function(req, res){
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true')
+		filteredTodos = _.where(todos, {completed: true});
+	else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false')
+		filteredTodos = _.where(todos, {completed: true});
+
+	res.json(filteredTodos);
 });
 
 //GET /todos/id
