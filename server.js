874aef655f16cd25ecb4e7ba06f2cd8ef9b22405
Siddarth Patel <siddarthpatel97@gmail.com>
2018-04-24 15:08:34 +0530

    add description query params

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/', function(req, res){
 	res.send('Todo API root');
 });
 
-//GET /todos?completed=true
+//GET /todos?completed=true&q=work
 app.get('/todos', function(req, res){
 	var queryParams = req.query;
 	var filteredTodos = todos;
@@ -37,7 +37,13 @@ app.get('/todos', function(req, res){
 	if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true')
 		filteredTodos = _.where(todos, {completed: true});
 	else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false')
-		filteredTodos = _.where(todos, {completed: true});
+		filteredTodos = _.where(todos, {completed: false});
+
+	if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0){
+		filteredTodos = _.filter(filteredTodos, function(todo){
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
+	}
 
 	res.json(filteredTodos);
 });
