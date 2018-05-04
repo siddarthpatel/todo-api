fbaf4062826a557d9dcd5cbb7d57a70421849e08
Siddarth Patel <siddarthpatel97@gmail.com>
2018-05-04 16:48:32 +0530

    generate login authorization token

--- server.js
+++ server.js
@@ -126,7 +126,8 @@ app.post('/users/login', function(req, res){
 	var body = _.pick(req.body, 'email', 'password');
 	
 	db.user.authenticate(body).then(function(user){
-		res.json(user.toPublicJSON());
+		//res.json(user.toPublicJSON());
+		res.header('Auth', user.generateToken('authentication')).json(user.toPublicJSON());
 	}, function(e){
 		res.status(401).json(e);
 	});
